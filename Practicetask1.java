753184b0d46c8e1f71dbe1eeee98683a84e26a46
Nandini Patil <nandinip632@gmail.com>
2025-10-01 11:33:48 +0530

    Add files via upload

---
+++ Practicetask1.java
@@ -0,0 +1,20 @@
+package com.PracticeDaily;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class Practicetask1 {
+	public static void main(String[] args) {
+		ArrayList <String> list=new ArrayList<String>();
+		list.add("Nandini");
+		list.add("Punam");
+		list.add("Kiran");
+		
+		System.out.println(list.iterator());
+		//System.out.println(list.get(0));
+		
+		
+	
+	
+	}
+}
