3432e30fb487e303a71300909e33989464be2bee
Nandini Patil <nandinip632@gmail.com>
2025-11-22 12:32:39 +0530

    Add files via upload

---
+++ LastComapare.java
@@ -0,0 +1,17 @@
+package com.codes;
+
+import java.util.Comparator;
+
+public class LastComapare implements Comparator<Student>{
+	@Override
+	public int compare(Student s1, Student s2) {
+		if(s1.rollno==s2.rollno) 
+			return 0;
+		else if(s1.rollno<s2.rollno)
+			return 1;
+		else
+			return -1;
+	}
+	
+
+}
---
+++ SortByKeys.java
@@ -0,0 +1,21 @@
+package com.codes;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class SortByKeys {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<String, Integer> map=new TreeMap<String, Integer>();
+		map.put("C", 19);
+		map.put("Z", 10);
+		map.put("T", 91);
+		map.put("M", 67);
+		map.put("G", 50);
+		System.out.println(map);
+		
+
+	}
+
+}
